de1e21cfa1c5a125c8102e749962d630e3ddf2e5
Omarbakr13 <168288302+Omarbakr13@users.noreply.github.com>
2024-09-28 22:18:15 +0300

    bakr work 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState } from 'react';
+import TourForm from './components/TourForm';
+import TourList from './components/TourList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [selectedTour, setSelectedTour] = useState(null);
+    const [refreshTours, setRefreshTours] = useState(false);
+
+    return (
+        <div>
+            <h1>Tour Guide Management</h1>
+            <TourForm refreshTours={() => setRefreshTours(!refreshTours)} />
+            <TourList setSelectedTour={setSelectedTour} refreshTours={refreshTours} />
+        </div>
+    );
+};
 
 export default App;
