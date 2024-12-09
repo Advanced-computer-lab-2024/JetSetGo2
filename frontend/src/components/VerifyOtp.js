325b4286a53563a8e03d4910f88930e6fd022d92
allamzoo <161718679+allamzoo@users.noreply.github.com>
2024-12-09 07:37:44 +0200

    arabnaaaaaaaa

--- frontend/src/components/VerifyOtp.js
+++ frontend/src/components/VerifyOtp.js
@@ -1,11 +1,18 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import '../css/VerifyOtp.css';
 
 const VerifyOtp = () => {
   const [otp, setOtp] = useState("");
   const [message, setMessage] = useState("");
   const [error, setError] = useState("");
   const navigate = useNavigate();
+  useEffect(() => {
+    document.body.classList.add("login-body");
+    return () => {
+      document.body.classList.remove("login-body");
+    };
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -22,21 +29,35 @@ const VerifyOtp = () => {
   };
 
   return (
-    <div>
-      <h2>Verify OTP</h2>
-      <form onSubmit={handleSubmit}>
-        <label>Enter OTP:</label>
-        <input
-          type="text"
-          value={otp}
-          onChange={(e) => setOtp(e.target.value)}
-          placeholder="Enter OTP"
-          required
-        />
-        <button type="submit">Verify OTP</button>
-      </form>
-      {message && <p style={{ color: "green" }}>{message}</p>}
-      {error && <p style={{ color: "red" }}>{error}</p>}
+    <div className="verify-otp-body">
+      <div className="verify-otp-container">
+      <div className="verify-otp-left">
+  <button onClick={() => navigate("/request-otp")} className="back-button">BACK</button>
+  <div className="verify-otp-caption"></div>
+
+
+
+
+          <div className="verify-otp-caption"></div>
+        </div>
+        <div className="verify-otp-right">
+          <h2>Enter the OTP</h2>
+          <form className="verify-otp-form" onSubmit={handleSubmit}>
+            <div className="verify-otp-form-group">
+              <input
+                type="text"
+                value={otp}
+                onChange={(e) => setOtp(e.target.value)}
+                placeholder="Enter OTP"
+                required
+              />
+            </div>
+            <button type="submit">Verify OTP</button>
+          </form>
+          {message && <p style={{ color: "green" }}>{message}</p>}
+          {error && <p style={{ color: "red" }}>{error}</p>}
+        </div>
+      </div>
     </div>
   );
 };
